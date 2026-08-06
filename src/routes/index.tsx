d018f44750524bd8487e533dc8f5a455d6bc47da
andersonlizarazo <dev@andersonlizarazo.com>
2026-08-06 16:52:51 -0500

    Show WIP status

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,22 +4,21 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 export default component$(() => {
   return (
     <>
-      <h1>Hi 👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </div>
+      <main>
+        <h1>Hi 👋</h1>
+        <p> This is a work in progress... </p>
+      </main>
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Anki Warm Template",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content:
+        "An Anki template generator that allows you to apply a warm-themed template to your cards",
     },
   ],
 };
